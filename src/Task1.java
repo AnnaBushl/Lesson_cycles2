53d1f13e3636062d5060befe49a078c85321a68a
Ann <ann.bushl2444@gmail.com>
2022-05-15 20:53:12 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,13 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 9){
+            i ++;
+            System.out.print(i + " ");
+        }
+        System.out.println ();
+        for (;i > 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
